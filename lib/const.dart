0048989e06fa0da4d56f2f1949f2b101c6a4edee
Mayank Bandil <63606264+mayanknitb11@users.noreply.github.com>
2020-09-05 15:37:25 +0530

    working

---
+++ lib/const.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+final themeColor = Colors.blue[700];
+final primaryColor = Color(0xff203152);
+final greyColor = Color(0xffaeaeae);
+final greyColor2 = Color(0xffE8E8E8);
